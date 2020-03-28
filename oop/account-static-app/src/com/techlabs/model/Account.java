b2f0a36a2eb063d7588ada0979ff98ea5b6cb2ce
Tejashree Wagh <waghtejashree@gmail.com>
2020-03-28 16:22:16 +0530

    account-static-app part5

--- oop/account-static-app/src/com/techlabs/model/Account.java
+++ oop/account-static-app/src/com/techlabs/model/Account.java
@@ -3,11 +3,22 @@ package com.techlabs.model;
 public class Account {
 	private double balance;
 	private static int MINIMUM_BALANCE;
+	private static int numOfTransactions ;
+	
+	static {
+		numOfTransactions = 0;
+	}
 
 	public void withdraw(double amount) {
 		if (balance - amount > MINIMUM_BALANCE) {
 			balance = balance - amount;
+			numOfTransactions++;
 		}
+	}
+	
+	public void deposit(double amount) {
+			balance = balance + amount;
+			numOfTransactions++;
 	}
 
 	public static void setMinimumBalance(int minimumBalance) {
@@ -20,6 +31,10 @@ public class Account {
 
 	public void setBalance(double balance) {
 		this.balance = balance;
+	}
+	
+	public static int getNumOfTransactions() {
+		return numOfTransactions;
 	}
 
 }
--- oop/account-static-app/src/com/techlabs/model/test/AccountTest.java
+++ oop/account-static-app/src/com/techlabs/model/test/AccountTest.java
@@ -5,12 +5,21 @@ import com.techlabs.model.Account;
 public class AccountTest {
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Test Case 1 :");
+		Account.setMinimumBalance(1000);
 		Account a1 = new Account();
-		a1.setMinimumBalance(1000);
 		a1.setBalance(123456);
 		a1.withdraw(345);
 		System.out.println("Total balance = " + a1.getBalance());
 		
+		System.out.println("Test Case 2 :");
+		a1.deposit(100);
+		a1.withdraw(50);
+		a1.deposit(50);
+		Account a2 = new Account();
+		a2.withdraw(100);
+		System.out.println(Account.getNumOfTransactions());
 	}
 
 }
