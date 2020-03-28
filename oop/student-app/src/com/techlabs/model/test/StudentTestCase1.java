44504a960c8404803fcfb5281a3643dcda7ce328
Tejashree Wagh <waghtejashree@gmail.com>
2020-03-28 14:23:47 +0530

    ---

--- oop/student-app/src/com/techlabs/model/test/StudentTestCase1.java
+++ oop/student-app/src/com/techlabs/model/test/StudentTestCase1.java
@@ -14,18 +14,6 @@ public class StudentTestCase1 {
 		Student s3 = new Student(101, "lmno", 8.0f);
 		System.out.println(s1.getStudentCount());
 
-		System.out.println("Test Case 2:");
-		StudentCase2 c1 = new StudentCase2(101, "abc");
-		System.out.println(s1.getStudentCount());
-		StudentCase2 c2 = new StudentCase2(101, "xyz");
-		System.out.println(s2.getStudentCount());
-		StudentCase2 c3 = new StudentCase2(101, "lmno", 8.0f);
-		System.out.println(s3.getStudentCount());
-
-		System.out.println("Test Case 3:");
-		StudentCase3 obj1 = new StudentCase3(101, "abc");
-		StudentCase3 obj2 = new StudentCase3(101, "xyz");
-		StudentCase3 obj3 = new StudentCase3(101, "lmno", 8.0f);
 	}
 
 }
